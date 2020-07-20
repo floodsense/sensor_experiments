91ce1a0aec31124fe521410308cd37ad1af535e0
Praneethsvch <58634765+Praneethsvch@users.noreply.github.com>
2020-07-20 14:29:26 -0400

    Update loggingMaxbotixontoSDcard.ino

--- src/misc/featherwing/SD card logging/loggingMaxbotixontoSDcard.ino
+++ src/misc/featherwing/SD card logging/loggingMaxbotixontoSDcard.ino
@@ -8,58 +8,16 @@
 
 long distance = 0;
 long duration = 0;
+
 File logfile;
 
-// blink out an error code
-void error(uint8_t errno) {
-  while (1) {
-    uint8_t i;
-    for (i = 0; i < errno; i++) {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
-      delay(100);
-    }
-    for (i = errno; i < 10; i++) {
-      delay(200);
-    }
-  }
-}
-
 void setup() {
+
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-   digitalWrite(8, HIGH);
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(10)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");
-
-   char filename[15];
-  strcpy(filename, "/SENSOR00.TXT");
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[7] = '0' + i / 10;
-    filename[8] = '0' + i % 10;
-    // create if does not exist, do not open existing, write, sync after write
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-  logfile = SD.open(filename, FILE_WRITE);
-  if ( ! logfile ) {
-    Serial.print("Couldnt create ");
-    Serial.println(filename);
-    error(3);
-  }
-  Serial.print("Writing to ");
-  Serial.println(filename);
 
   pinMode(13, OUTPUT);
   pinMode(triggerPin, OUTPUT);
@@ -68,8 +26,8 @@ void setup() {
 
   delay(3000);
   Serial.println("Setup Ready!");
-  digitalWrite(8, LOW);
 }
+
 void read_sensor() {
 
   digitalWrite(triggerPin, HIGH);
@@ -87,11 +45,54 @@ void print_data() {
   Serial.println(" mm");
 }
 
+
+void writeToSDCard() {
+
+  // The CS pin on Feather m0 LoRa board is shared by the radio module and should be pulled HIGH
+  // before using and set back to LOW for the radio to work.
+  digitalWrite(8, HIGH);
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+  } else {
+    Serial.println("initialization done.");
+    char filename[15];
+    strcpy(filename, "/SENSOR00.TXT");
+
+    //  for (uint8_t i = 0; i < 100; i++) {
+    //    filename[7] = '0' + i / 10;
+    //    filename[8] = '0' + i % 10;
+    //    // create if does not exist, do not open existing, write, sync after write
+    //    if (! SD.exists(filename)) {
+    //      break;
+    //    }
+    //  }
+
+    logfile = SD.open(filename, FILE_WRITE);
+
+    if ( ! logfile ) {
+      Serial.print("error opening ");
+      Serial.println(filename);
+    }
+
+    logfile.print("Dist = "); logfile.println(distance);
+    Serial.println("Writing to SD Card");
+    Serial.print("Dist = "); Serial.println(distance);
+    //flush after write but to note it takes extra power
+    logfile.flush();
+    logfile.close();
+    digitalWrite(8, LOW);
+  }
+}
+
 void loop() {
 
+  // Read the sensor data
   read_sensor();
-  logfile.print("Dist = "); logfile.println(distance);
-  Serial.print("Dist = "); Serial.println(distance);
+
+  //Write to SD Card
+  writeToSDCard();
 
   delay(1000);
 }
